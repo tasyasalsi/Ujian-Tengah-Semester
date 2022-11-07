08c0ea6a33b744847a2a6cf612bc10df3f9f71d8
salsyii <114418450+tasyasalsi@users.noreply.github.com>
2022-11-07 13:13:23 +0700

    Create Jawaban3.py

---
+++ Jawaban3.py
@@ -0,0 +1,9 @@
+print ('================')
+print ('  Program Quiz  ')
+print ('================')
+
+nama = input('\nMasukkan Nama: ')
+NIM = input ('Masukkan NIM: ')
+
+print('\nNama: ', nama)
+print('NIM: ', NIM)
